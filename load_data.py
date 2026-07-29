4dca0397e54fb1d5883356a2952697bee8a12c59
avi-nz <avishai@narsey.com>
2026-07-30 11:44:09 +1200

    added the data and a file which we can easily call the data

---
+++ load_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def load_locations():
+    return pd.read_csv("data/FoodstuffsLocations.csv")
+
+def load_demand():
+    return pd.read_csv("data/FoodstuffsDemand2026.csv")
+
+def load_durations():
+    return pd.read_csv("data/FoodstuffsDugit arations2026.csv")
